38ccbbb575256c44674fe789aa8881983f57099e
ScithiansCloud <cornelius.laag@gmail.com>
2025-06-26 15:17:31 +0200

    updated settings

--- settings.py
+++ settings.py
@@ -4,7 +4,19 @@
 ## imports
 import numpy as np
 
-def init():
+def init(Csi):
+
+    ##########################################################
+
+    #Simulation 
+    global N 
+    N = 1
+
+    global nsteps
+    nsteps = 1
+
+    global nsave
+    nsave = 1
 
     # constants
     global kB
@@ -24,34 +36,48 @@ def init():
     global sig 
     sig = 1
 
-    global delta 
-    delta = 1
-
-    global rho 
-    rho = 1
-
     global Cs 
-    Cs = 1 ##maybe we handle this not in settings because we shoudl vary it a lot idk...
+    Cs = Csi ##maybe we handle this not in settings because we shoudl vary it a lot idk...
 
     global Z 
     Z = 1
 
+    global gaus_var
+    gaus_var = 1
+
     ## derived constants
 
-    '''L, kappa d, Zprime, A , tau LD'''
+    global rho 
+    rho = 0.005*sig**(-3)
+
+    global L 
+    L = (N/rho)**(1/3)
+
+    global delta 
+    delta = sig*1e-2
+
+    global lambda_B 
+    lambda_B = sig/100
+    
+    global kappa_D
+    kappa_D = np.sqrt(8*np.pi *lambda_B* Csi  )
+
+    global A 
+    A = 0.1*kB*T
+
+    global Zprime
+    Zprime = Z*np.exp(kappa_D*sig/2)/(1+kappa_D*sig/2)
+
+    global tau
+    tau = sig*sig/(kB*T)*xi
+
+    global delta_t
+    delta_t= 1e-3*tau
+
+    '''L, kappa d, Zprime, A , tau LD, lambda_B'''
 
 
-    ##########################################################
 
-    #Simulation 
-    global N 
-    N = 1
-
-    global nsteps
-    nsteps = 1
-
-    global nsave
-    nsave = 1
 
 
     
