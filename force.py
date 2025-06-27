9ff7d73e099a4b23ca407ac21374f114fd7baa17
ScithiansCloud <cornelius.laag@gmail.com>
2025-06-27 08:11:49 +0200

    added force.py

---
+++ force.py
@@ -0,0 +1,68 @@
+'''here we will calculate the forces'''
+
+import numpy as np
+from numba import njit, prange
+
+
+@njit(parallel=True)
+def forceLJ(x, y, z, xlo, L, N, sig, delta, A, m, Zprimesqrd, lambda_B, kappa_D, kbT):
+    
+    fx = np.zeros(shape=len(x))
+    fy = np.zeros(shape=len(x))
+    fz = np.zeros(shape=len(x))
+
+
+
+    for i in range(N-1):
+        for j in range(i+1, N):
+
+            rijx = pbc(x[i], x[j], 0, L)
+            rijy = pbc(y[i], y[j], 0, L)
+            rijz = pbc(z[i], z[j], 0, L)
+            
+            r2 = rijx * rijx + rijy * rijy + rijz * rijz
+            r = np.sqrt(r2)
+
+            if r > (sig + delta):
+                '''calculate vdw + elec interaction'''
+
+                VDW = A*sig/(24*m*(r-sig)**2)
+                EL = -Zprimesqrd*lambda_B*np.exp(-kappa_D*r)*(1/r+kappa_D)/(m*r**2)
+                SUM = VDW + EL
+
+                fx[i] -= SUM* rijx
+                fy[i] -= SUM* rijy
+                fz[i] -= SUM* rijz
+                fx[j] += SUM* rijx
+                fy[j] += SUM* rijy
+                fz[j] += SUM* rijz
+            
+            else:
+                '''calculate lj interacion'''
+                LJ = 8*kbT/m*((0.27993599999999996/r**8-0.02612138803199999/r**14))
+
+                fx[i] -= LJ* rijx
+                fy[i] -= LJ* rijy
+                fz[i] -= LJ* rijz
+                fx[j] += LJ* rijx
+                fy[j] += LJ* rijy
+                fz[j] += LJ* rijz
+
+
+### doppelcheckt mla jmd die ableitung der potentiale ob das so sinn macht und algebraisch auch stimmt (und vorzeichen bin ihc mir nicht 100% sicher aber ist mal ein anfang :)
+
+
+
+@njit  
+def pbc(xi, xj, xlo, xhi):
+    
+    l = xhi-xlo
+    
+    xi = xi % l
+    xj = xj % l
+    
+    rij = xj - xi  
+    if abs(rij) > 0.5*l:
+        rij = rij - np.sign(rij) * l 
+        
+    return rij
---
+++ initialize.py
@@ -0,0 +1,5 @@
+'''idk what ur opninion is here we could go for some random initialization
+ (maybe place them one by one such that they do not overlap) or on a grid (i think its not specified in the exercies)
+ 
+ Grid doesnt work that well because N doest have a 3rd root :/
+ '''
