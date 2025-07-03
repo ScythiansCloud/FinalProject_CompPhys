1f422b87adb47f6a4fb6e6163b1f0660c96a6886
ScithiansCloud <cornelius.laag@gmail.com>
2025-07-03 14:21:35 +0200

    cleaned working tree

--- utilities/output.py
+++ utilities/output.py
@@ -16,4 +16,7 @@ def WriteTrajectory3d(fileoutput, itime, x, y, z, settings):
     
     for i in range(0, settings.N):
         fileoutput.write("%i %i %e %e %e \n" % (i, i, x[i] % settings.L, y[i] % settings.L, z[i] % (settings.L)))
+
+def WriteunwrappedState(fileoutput, itime, x,y,z,vx,vy,vz):
+    fileoutput.write(str(itime)+ ' '+ str(x)+' '+ str(y)+' '+ str(z)+' '+ str(vx)+' '+ str(vy)+' '+ str(vz)+ '\n')
     
