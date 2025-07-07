c0a659c3bfc04b61e6d9e195cbda80480d307c6b
Coecer <j.maier.borst@gmail.com>
2025-07-07 18:58:28 +0200

    did run task 3

--- utilities/simulation.py
+++ utilities/simulation.py
@@ -91,24 +91,24 @@ def Simulation3(outdir, write, Traj_name, everyN, random_seed, settings): # forc
     # random seed for reproducibility
     np.random.seed(settings.random_seed)
     x, y, z, vx, vy, vz = initialize.InitializeAtoms(settings.Cs, settings.random_seed, settings)
-    x,y,z, vx, vy, vz = update.update(True, x, y, z, vx, vy, vz, settings.L, settings.N, settings.sig,
+    x,y,z, vx, vy, vz = update.update(False, x, y, z, vx, vy, vz, settings.L, settings.N, settings.sig,
                                       settings.delta, settings.A, settings.m, settings.Zprimesqrd,
                                       settings.lambda_B, settings.kappa_D, settings.kBT, settings.xi,
                                       settings.delta_t, settings.gaus_var, settings.random_seed)
     
 
+    # open equilibration files
     if write:
-        fileoutput_eq = open(outdir / (Traj_name + str(everyN) + '_nsteps_' + str(settings.nsteps)), "w")
+        fileoutput_eq = open(outdir / (Traj_name + "_eq_" + str(everyN) + '_nsteps_' + str(settings.nsteps)), "w")
         fileoutput_eq_unwrapped = open(outdir / (Traj_name+ 'unwrapped'+ str(everyN) + '_nsteps_' + str(settings.nsteps)), "w")
         output.WriteTrajectory3d(fileoutput_eq, 0,x,y,z, settings) 
         output.WriteunwrappedState(fileoutput_eq_unwrapped,0,x,y,z,vx,vy,vz)
         # output.WriteTrajectory3d(fileoutput_prod, 0,x,y,z) 
     
-    Ngr = 0
-    nbins = int(settings.L/2 / settings.dr)
-    hist = np.zeros(nbins) 
-    for i in tqdm(range(settings.nsteps)):
-        x,y,z, vx, vy, vz = update.update(True, x, y, z, vx, vy, vz, settings.L, settings.N, settings.sig,
+    # equilibration run
+    logging.info(f"Starting with equilibration run of {settings.nsteps_eq} steps.")
+    for i in tqdm(range(settings.nsteps_eq)):
+        x,y,z, vx, vy, vz = update.update(False, x, y, z, vx, vy, vz, settings.L, settings.N, settings.sig,
                                         settings.delta, settings.A, settings.m, settings.Zprimesqrd,
                                         settings.lambda_B, settings.kappa_D, settings.kBT, settings.xi,
                                         settings.delta_t, settings.gaus_var, settings.random_seed)
@@ -119,6 +119,33 @@ def Simulation3(outdir, write, Traj_name, everyN, random_seed, settings): # forc
                 # logging.info(force.temperature(vx,vy,vz,settings=settings))
                 output.WriteTrajectory3d(fileoutput_eq, i,x,y,z, settings)
                 output.WriteunwrappedState(fileoutput_eq_unwrapped,i,x,y,z,vx,vy,vz)
+    
+
+    # open PRODUCTION files
+    if write:
+        fileoutput_prod = open(outdir / (Traj_name + str(everyN) + '_nsteps_' + str(settings.nsteps)), "w")
+        fileoutput_prod_unwrapped = open(outdir / (Traj_name+ 'unwrapped'+ str(everyN) + '_nsteps_' + str(settings.nsteps)), "w")
+        output.WriteTrajectory3d(fileoutput_prod, 0,x,y,z, settings) 
+        output.WriteunwrappedState(fileoutput_prod_unwrapped,0,x,y,z,vx,vy,vz)
+        # output.WriteTrajectory3d(fileoutput_prod, 0,x,y,z) 
+    
+    # production run
+    logging.info(f"Starting with production run of {settings.nsteps} steps.")
+    Ngr = 0
+    nbins = int(settings.L/2 / settings.dr)
+    hist = np.zeros(nbins) 
+    for i in tqdm(range(settings.nsteps)):
+        x,y,z, vx, vy, vz = update.update(False, x, y, z, vx, vy, vz, settings.L, settings.N, settings.sig,
+                                        settings.delta, settings.A, settings.m, settings.Zprimesqrd,
+                                        settings.lambda_B, settings.kappa_D, settings.kBT, settings.xi,
+                                        settings.delta_t, settings.gaus_var, settings.random_seed)
+        
+        # save shit every n
+        if i % everyN == 0:
+            if write:
+                # logging.info(force.temperature(vx,vy,vz,settings=settings))
+                output.WriteTrajectory3d(fileoutput_prod, i,x,y,z, settings)
+                output.WriteunwrappedState(fileoutput_prod_unwrapped,i,x,y,z,vx,vy,vz)
 
 
             hist = update.update_hist(hist, x,y,z, settings.dr, settings.N, settings.L)
